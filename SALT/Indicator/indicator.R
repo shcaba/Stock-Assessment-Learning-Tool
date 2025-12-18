32edbdca5b4e5052d8cccf58c6f24074652efeb4
shcaba <jason.cope@noaa.gov>
2025-12-17 17:07:48 -0800

    update to indicator module

--- SALT/Indicator/indicator.R
+++ SALT/Indicator/indicator.R
@@ -3,6 +3,7 @@ library(bslib)
 library(ggplot2)
 library(dplyr)
 library(shinyWidgets)
+library(viridis)
 
 ui <- page_sidebar(
   title = "Time Series Average vs Target Comparison",
@@ -14,9 +15,28 @@ ui <- page_sidebar(
                 label = 'Choose A Stock',
                 choices = c("A","B","C"),
                 options = list(`style` = "btn-info")),
+    
+    uiOutput("data.indicator"),
+    
+    # checkboxGroupButtons(
+    #   inputId = "data.id",
+    #   label = "Which data to consider: ",
+    #   choices = c("Catch", "Index", "Mean Length"),
+    #   size="sm"
+    # ),
+    
+    # prettyCheckboxGroup( # or prettyRadioButtons
+    #   inputId = "data.id",
+    #   label = "Which data to consider",
+    #   choices = c("Catch", "Index", "Mean Length"),
+    #   outline = TRUE,
+    #   plain = TRUE,
+    #   status = "primary",
+    #   icon = icon("check")
+    # ),
 
     # Input for target value
-    h5("Select the years to use for each indicator by data set"),
+    h5("Select indicator and reference point years"),
     textInput(
       "Ct_I_yrs",
       "Indicator years:",
@@ -75,22 +95,13 @@ ui <- page_sidebar(
   
   # Main panel with results
   layout_columns(
-    col_widths = c(6, 6),
-    
-    # Summary statistics card
-    card(
-      card_header("Summary Statistics"),
-      tableOutput("summary_stats")
-    ),
     card(
       card_header("General Management Procedure"),
-       withMathJax(),
-       div(
-         style = "text-align: center; font-size:24px; margin: 20px 0;",
-         "$$\\ MM_{y} = \\ MM_{y-z} \\cdot \\ Modifier$$"
-       ),
-      #p("MM_(y+1) = MM_Y * Modifier"),
-      #br(),
+      withMathJax(),
+      div(
+        style = "text-align: center; font-size:20px; margin: 10px 0;",
+        "$$\\ MM_{y} = \\ MM_{y-z} \\cdot \\ Modifier$$"
+      ),
       p("Where:"),
       tags$ul(
         tags$li("MM = Management Metric. Examples are catch limits, effort, etc."),
@@ -105,30 +116,55 @@ ui <- page_sidebar(
       p("The control rule would then be the Modifier used in the general equation."),
       p("An example of a simple control rule based on an Indicator (I) and Reference Point (RP) is:."),
       div(
-        style = "text-align: center; font-size:24px; margin: 20px 0;",
+        style = "text-align: center; font-size:20px; margin: 10px 0;",
         "$$\\ CR = \\frac{I}{RP}$$"
       ),
-      
+      p("Indicators can be model-free (using the data directly) or model-based (from a stock assessment) "),
     ),
     
-    # Comparison results card
+    layout_columns(
+    # Time series plot
+      card_header("Data Time Series"),
+        card(
+          #plotlyOutput("stock_time_series_Ct", height = "200px")
+          uiOutput("stock_time_series_Ct_ui")
+        ),
+      
+        card(
+      #plotlyOutput("stock_time_series_Index", height = "200px")
+      uiOutput("stock_time_series_Index_ui")
+        ),
+
     card(
-      card_header("Target Comparison"),
-      tableOutput("comparison_results")
+      #plotlyOutput("stock_time_series_Lt", height = "200px")
+      uiOutput("stock_time_series_Lt_ui")
+    ),
+
+        col_widths = c(12,12,12)
     )
   ),
-  
-  # Time series plot
+
+  # Summary statistics card
+  layout_columns(
   card(
-    card_header("Time Series Plot with Average and Target"),
-    plotOutput("time_series_plot", height = "400px")
+    card_header("Summary Statistics"),
+    tableOutput("summary_stats")
   ),
   
-  # Distribution plot
+  # Comparison results card
   card(
-    card_header("Value Distribution"),
-    plotOutput("distribution_plot", height = "300px")
+    card_header("Target Comparison"),
+    tableOutput("comparison_results")
+  ),
+  col_widths = c(6,6)
   )
+  
+  
+  # Distribution plot
+  # card(
+  #   card_header("Value Distribution"),
+  #   plotOutput("distribution_plot", height = "300px")
+  # )
 )
 
 
@@ -139,20 +175,133 @@ server <- function(input, output, session) {
   
   # Reactive function to parse and process time series data
   
+  
+  
   stock.data <- readRDS("stock_data.RDS")
-  observe({print(input$stock.choice)})
-  browser()
+  data.colors<-viridis(3)
   
-  
-  stock.data.choice<-reactive({
-    stock.data.choice<-subset(stock.data,Stock=="A")
+  data.sub<-reactive({
+        data.sub<-subset(stock.data,Stock==input$stock.choice)
       })
   
-  input$stock.choice
-  output$time_series_plot <- renderPlot({
-        
-    })
+  output$data.indicator <- renderUI({
+    checkboxGroupButtons(
+      inputId = "data.id",
+      label = "Which data to consider: ",
+      choices = c("Catch", "Index", "Mean Length"),
+      size="sm"
+    )
+  })
+ 
+#Indicator plots
+  output$stock_time_series_Ct_ui<-renderUI({
+    if(!is.null(input$data.id) & any(input$data.id=="Catch"))
+    {
+      output$stock_time_series_Ct <- renderPlotly({
+        data.plot<-data.sub()
+        ggplot(data.plot,aes(Year, Catch))+
+          geom_line(col=data.colors[1])+
+          geom_point(col=data.colors[1])+
+          theme_bw()+
+          xlab("Year")+
+          ylab("Total Catch")
+      })
+      
+    }
+  })
+
+  output$stock_time_series_Index_ui<-renderUI({
+    if(!is.null(input$data.id) & any(input$data.id=="Index"))
+    {
+      output$stock_time_series_Ct <- renderPlotly({
+        data.plot<-data.sub()
+        ggplot(data.plot,aes(Year, Index))+
+          geom_line(col=data.colors[2])+
+          geom_point(col=data.colors[2])+
+          theme_bw()+
+          xlab("Year")+
+          ylab("Index")
+      })
+      
+    }
+  })
   
+  output$stock_time_series_Lt_ui<-renderUI({
+    if(!is.null(input$data.id) & any(input$data.id=="Mean Length"))
+    {
+      if(input$stock.choice=="A")
+      {
+       M.in<-0.0375
+       Linf.in<-60.1
+       k.in<-0.08
+       t0.in<- -0.55
+       L50.in<- 46.5
+      }
+
+      if(input$stock.choice=="B")
+      {
+        M.in<-0.068
+        Linf.in<-42.8
+        k.in<-0.13
+        t0.in<- -0.94
+        L50.in<- 29
+      }
+      
+      if(input$stock.choice=="C")
+      {
+        M.in<-0.145
+        Linf.in<-53
+        k.in<-0.143
+        t0.in<- -0.07
+        L50.in<- 42
+      }
+      
+      
+      
+            output$stock_time_series_Ct <- renderPlotly({
+        data.plot<-data.sub()
+        ggplot(data.plot,aes(Year, Mean.Length))+
+          geom_line(col=data.colors[3])+
+          geom_point(col=data.colors[3])+
+          theme_bw()+
+          xlab("Year")+
+          ylab("Mean Length")+
+          geom_hline(aes(yintercept = Linf.in),col="blue",linetype = "longdash")+
+          geom_hline(aes(yintercept = L50.in),col="purple",linetype = "dash")+
+          ylim(0,NA)+
+          annotate("text",x=1,y=Linf.in+0.05*Linf.in,label="Linf",col="blue")+
+          annotate("text",x=1,y=L50.in+0.05*L50.in,label="Lmat50%",col="purple")
+            })
+      
+    }
+  })
+  
+  # output$stock_time_series_Ct <- renderPlotly({
+  #   data.plot<-data.sub()
+  #   ggplot(data.plot,aes(Year, Catch))+
+  #     geom_line(col=data.colors[1])+
+  #     geom_point(col=data.colors[1])+
+  #     theme_bw()
+  # })
+  # 
+  # output$stock_time_series_Index <- renderPlotly({
+  #   data.plot<-data.sub()
+  #   ggplot(data.plot,aes(Year, Index))+
+  #     geom_line(col=data.colors[2])+
+  #     geom_point(col=data.colors[2])+
+  #     theme_bw()
+  # })
+  # 
+  # output$stock_time_series_Lt <- renderPlotly({
+  #   data.plot<-data.sub()
+  #   ggplot(data.plot,aes(Year, Mean.Length))+
+  #     geom_line(col=data.colors[3])+
+  #     geom_point(col=data.colors[3])+
+  #     theme_bw()
+  #   
+  # })
+  # 
+    
   processed_data <- eventReactive(input$update_analysis, {
     # Parse the input string
     values_text <- gsub("\\s", "", input$time_series) # Remove whitespace
